9db4b7751914364176f3c3157cf52dfcdfc30ee2
de5893 <18250819@qq.com>
2024-05-10 09:58:36 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
-# 创建应用实例
 import sys
+from wxcloudrun import create_app
 
-from wxcloudrun import app
+app = create_app()
 
 # 启动Flask Web服务
 if __name__ == '__main__':
-    app.run(host=sys.argv[1], port=sys.argv[2])
+    if len(sys.argv) > 2:
+        host = sys.argv[1]
+        port = int(sys.argv[2])
+    else:
+        host = '0.0.0.0'
+        port = 5000  # 默认端口号，可以根据需要调整
+
+    app.run(host=host, port=port)

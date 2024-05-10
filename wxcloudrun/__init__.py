0a55eb1afc8d496c4f009f6a0afc132a94b50112
de5893 <18250819@qq.com>
2024-05-10 09:44:58 +0800

    Update __init__.py

--- wxcloudrun/__init__.py
+++ wxcloudrun/__init__.py
@@ -1,24 +1,28 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import pymysql
-import config
+从烧瓶导入烧瓶
+从flask_sqlalchemy导入SQLAlchemy
+导入pymysql
+导入配置
 
 # 因MySQLDB不支持Python3，使用pymysql扩展库代替MySQLDB库
-pymysql.install_as_MySQLdb()
+pymysql。install_as_MySQLdb  (   ）
 
-# 初始化web应用
-app = Flask(__name__, instance_relative_config=True)
-app.config['DEBUG'] = config.DEBUG
+db = SQLAlchemy    (    )      # 初始化DB操作对象，不绑定app
 
-# 设定数据库链接
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{}:{}@{}/flask_demo'.format(config.username, config.password,
-                                                                             config.db_address)
+def     create_app    (    )：
+    # 初始化web应用
+    应用程序= Flask （ __name__，instance_relative_config = True）
+    应用程序。配置[   '调试'  ] =配置。调试
 
-# 初始化DB操作对象
-db = SQLAlchemy(app)
+    # 设置数据库链接
+    应用程序。config [  'SQLALCHEMY_DATABASE_URI'   ] = 'mysql://{}:{}@{}/flask_demo'。格式（配置.用户名，配置.密码，
+                                                                                 配置。数据库地址）
+    D b。init_app    ( app )      # 绑定app到db
 
-# 加载控制器
-from wxcloudrun import views
+    # 加载控制器
+    从wxcloudrun导入视图
+    ideas。init_app    ( app )      # 将View函数的注册移到views.py中的一个函数
 
-# 加载配置
-app.config.from_object('config')
+    # 加载配置
+    应用程序。配置。from_object   (  '配置' )
+
+    返回应用程序
